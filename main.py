86bd3c7c3dba2261cc8fde45170410a2599b682c
rjvb7424 <rjvb7424@leeds.ac.uk>
2024-11-25 10:56:03 +0000

    Software development finished!

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
+import random
 import requests
 
-COUNT = 20
-pokemon_names = []
-pokemon_heights = []
+def fetch_pokemon_name(pokemon_id: int):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+    return response.json()["name"]
 
-while COUNT != 0:
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{COUNT}")
-    if response.status_code == 200:
-        data = response.json()
-        name = data["name"]
-        height = data["height"]
-        pokemon_names.append(name)
-        pokemon_heights.append(height)
-    else:
-        print("Request failed")
-    COUNT -= 1
+def fetch_pokemon_database(database_size: int):
+    pokemon_database = []
+    while database_size > 0:
+        pokemon_name = fetch_pokemon_name(database_size)
+        pokemon_database.append(pokemon_name)
+        database_size -= 1
+    return pokemon_database
 
-COUNT = 19
-while COUNT != -1:
-    print(f"{pokemon_names[COUNT]} is {pokemon_heights[COUNT]} tall!")
-    COUNT -= 1
+pokemon_database = fetch_pokemon_database(50)
+
+def pick_pokemon(pokemon_database):
+    return random.choice(pokemon_database)
+
+picked_pokemon = pick_pokemon(pokemon_database)
+
+def name_for_underscores(picked_pokemon):
+    return ["_" for _ in picked_pokemon]
+
+hidden_pokemon = name_for_underscores(picked_pokemon)
+
+def hint_system(hidden_pokemon, picked_pokemon):
+    while True:
+        reveal_character = random.randint(0, len(hidden_pokemon) - 1)
+        if hidden_pokemon[reveal_character] == "_":
+            hidden_pokemon[reveal_character] = picked_pokemon[reveal_character]
+            break
+    return hidden_pokemon
+
+user_input = ""
+while user_input != picked_pokemon:
+    if ''.join(hidden_pokemon) == picked_pokemon:
+        break
+    print(f"\033[33m{''.join(hidden_pokemon)}\033[0m")
+    user_input = input("\033[31mWho's that Pokemon?!\033[0m ")
+    print()
+    hidden_pokemon = hint_system(hidden_pokemon, picked_pokemon)
+
+print(f"\033[33mIt's {picked_pokemon}!\033[0m")
