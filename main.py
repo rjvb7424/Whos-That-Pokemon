178587137b08a23e12773cdfbf1ba4960fd4ddaa
rjvb7424 <rjvb7424@leeds.ac.uk>
2024-11-25 10:16:03 +0000

    Software states for the first 20 Pokemons in the pokedex what there name is and how tall they are.

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import requests
 
 COUNT = 20
 pokemon_names = []
+pokemon_heights = []
 
 while COUNT != 0:
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{COUNT}")
     if response.status_code == 200:
         data = response.json()
         name = data["name"]
+        height = data["height"]
         pokemon_names.append(name)
+        pokemon_heights.append(height)
     else:
         print("Request failed")
     COUNT -= 1
 
-print(pokemon_names)
+COUNT = 19
+while COUNT != -1:
+    print(f"{pokemon_names[COUNT]} is {pokemon_heights[COUNT]} tall!")
+    COUNT -= 1
